1985e5c112a6cb4254310e7fb709cf180d5f2e7e
camilleNicole <camilleht@gmail.com>
2022-03-29 21:38:18 -0400

    update

--- models/index.js
+++ models/index.js
@@ -1,54 +1,55 @@
+const Skill = require('./Skill');
 const User = require('./User');
 const Job = require('./Job');
 const Employer = require('./Employer');
-const Skill = require('./Skill');
 const Message = require('./Message');
-
-
 // User-Skill one to many
-User.hasMany(Skill, {
-    foreignKey: 'skill_id'
+User.belongsToMany(Skill, {
+    through: 'skill_id',
+    as: 'skill'
 });
-
-Skill.belongsTo(User, {
-    foreignKey: 'skill_id'
+    // foreignKey: 'skill_id'
+Skill.belongsToMany(User, {
+    through: 'skill_id',
+    as: 'skill'
 });
-
+    // foreignKey: 'skill_id');
 // Skill-Job many to many
 Skill.belongsToMany(Job, {
-    foreignKey: 'skill_id'
+    through: 'skill_id',
+    as: 'jobskill'
 });
-
 Job.belongsToMany(Skill, {
-    foreignKey: 'skill_id'
+    through: 'skill_id',
+    as: 'jobskill'
 });
-
 // Employer to job one to many
 Employer.hasMany(Job, {
     foreignKey: 'employer_id'
 });
-
 Job.belongsTo(Employer, {
     foreignKey: 'employer_id'
 });
-
 // message to user/employer both many to one
 Message.belongsTo(User, {
     foreignKey: 'user_id'
 });
-
 Message.belongsTo(Employer, {
     foreignKey: 'employer_id'
 });
-
 User.hasMany(Message, {
     foreignKey: 'user_id'
 });
-
 Employer.hasMany(Message, {
     foreignKey: 'employer_id'
 });
-
-
 module.exports = { User, Employer, Skill, Job, Message };
+
+
+
+
+
+
+
+
 
