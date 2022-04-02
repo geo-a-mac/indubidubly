9d57024aa2454961cfb3881e0e8658919104f302
camilleNicole <camilleht@gmail.com>
2022-04-02 15:19:41 -0400

    fix for sign up form

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,51 +1,49 @@
 async function signupFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector('#username-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
-  const employer = document.querySelector('#employer').value;
-  const jobSeeker = document.querySelector('#job-seeker').value;
+  const employerSignUp = document.querySelector("#employer");
+  const jobSeekerSignUp = document.querySelector("#job-seeker");
 
-const url = document.querySelector('#employerURL').value;
-const skillId = document.querySelector('#job-seeker-skill').value;
+const url = document.querySelector("#employerURL").value;
+ 
+const radioButtons = document.querySelectorAll('input[name="skill"]');
+    let skill_id;
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            skill_id = radioButton.value;
+            break;
+        }
+    }
 
-// const javaScript = document.querySelector('#skillId1').value;
-// const cHashtag = document.querySelector('skillId2').value;
-// const frontEnd = document.querySelector('#skillId3').value;
-// const backEnd = document.querySelector('#skillId4').value;
-// const agile = document.querySelector('#skillId5').value;
-// const projectManagement = document.querySelector('#skillId6').value;
-// const Nodejs = document.querySelector('#skillId7').value;
-// const MySQL = document.querySelector('#skillId8').value;
-// const sequelize = document.querySelector('#skillId9').value;
-
-console.log(jobSeeker);
-
-  if(jobSeeker === "Job Seeker") {
-    console.log("user");
-    if (username && email && password && skillId) {
+  if(jobSeekerSignUp.checked) {
+    console.log(skill_id);
+    if (username && email && password && skill_id) {
       const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
           username,
           email,
           password,
-          skillId,
+          skill_id
         }),
         headers: { 'Content-Type': 'application/json' }
-      });
-      // check the response status
-      if (response.ok) {
-        document.location.replace('/dashboard');
-        return;
-      } else {
-        alert(response.statusText);
-      }
+      })
+      .then(response => {
+        if(response.ok) {
+          document.location.replace('/dashboard/');
+          alert("success");
+        } else {
+          alert(response.statusText);
+        }
+      })
+      .catch(err => console.log(err));
     }
-  } 
-  else if (employer === "Employer") {
+  }
+  else if (employerSignUp.checked) {
     console.log("employer");
     if (username && email && password && url) {
       const response = await fetch('/api/employers', {
@@ -57,96 +55,86 @@ console.log(jobSeeker);
           url,
         }),
         headers: { 'Content-Type': 'application/json' }
-      });
+      })
       // check the response status
-      if (response.ok) {
+    .then(response => {
+      if(response.ok) {
         document.location.replace('/dashboard');
-        return;
+        alert("success");
       } else {
         alert(response.statusText);
       }
-    }
+    })
+    .catch(err => console.log(err));
   }
-  console.log("test");
-  // if (username && email && password) {
-  //   const response = await fetch('/api/users', {
-  //     method: 'post',
-  //     body: JSON.stringify({
-  //       username,
-  //       email,
-  //       password
-  //     }),
-  //     headers: { 'Content-Type': 'application/json' }
-  //   });
-  //   // check the response status
-  //   if (response.ok) {
-  //     document.location.replace('/dashboard');
-  //   } else {
-  //     alert(response.statusText);
-  //   }
-  // }
+}
 }
 
 async function loginFormHandler(event) {
   event.preventDefault();
 
-  const email = document.querySelector('#email-login').value.trim();
-  const password = document.querySelector('#password-login').value.trim();
-  const employer = document.querySelector('#employer-sign-in');
-  const jobSeeker = document.querySelector('#job-seeker-sign-in');
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+  const employer = document.querySelector("#employer-sign-in");
+  const jobSeeker = document.querySelector("#job-seeker-sign-in");
 
-
-  if(jobSeeker.checked) {
+  if (jobSeeker.checked) {
     console.log(jobSeeker.value);
-    if(email && password) {
-      const response = await fetch('/api/users/login', {
-        method: 'post',
+    if (email && password) {
+      const response = await fetch("/api/users/login", {
+        method: "post",
         body: JSON.stringify({
           email,
-          password
+          password,
         }),
-        headers: { 'Content-Type': 'application/json'}
+        headers: { "Content-Type": "application/json" },
       })
-      .then(response => {
-        if(response.ok) {
-          document.location.replace('/dashboard');
-          alert("success");
-        } else {
-          alert(response.statusText);
-        }
+        .then((response) => {
+          if (response.ok) {
+            document.location.replace("/dashboard");
+            alert("success");
+          } else {
+            alert(response.statusText);
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+  if (employer.checked) {
+    console.log(employer.value);
+    if (email && password) {
+      const response = await fetch("/api/employers/login", {
+        method: "post",
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+        headers: { "Content-Type": "application/json" },
       })
-      .catch(err => console.log(err));
-  }  
- } 
- if (employer.checked) {
-   console.log(employer.value);
-   if(email && password) {
-     const response = await fetch('/api/employers/login', {
-       method: 'post',
-       body: JSON.stringify({
-         email,
-         password
-       }),
-       headers: {'Content-Type': 'application/json'}
-     })
-     .then(response => {
-        if(response.ok) {
-          document.location.replace('/dashboard');
-          alert("success");
-        } else {
-          alert(response.statusText);
-        }
-     })
-     .catch(err => console.log(err));
-   }
- }
-};
-
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+        .then((response) => {
+          if (response.ok) {
+            document.location.replace("/dashboard");
+            alert("success");
+          } else {
+            alert(response.statusText);
+          }
+        })
+        .catch((err) => console.log(err));
+    }
+  }
+}
 
 /*navbar click event*/
 $(document).on("click", ".action-buttons .dropdown-menu", function (e) {
   e.stopPropagation();
 });
+
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
+
+document
+  .querySelector("#signup")
+  .addEventListener("click", signupFormHandler);
+
+
